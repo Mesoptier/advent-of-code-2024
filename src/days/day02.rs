9058032df42157479c39f77cdeff64ba99bcb3d3
Koen Klaren <koen.klaren@gmail.com>
2024-12-02 18:09:35 +0100

    Simplify is_safe

--- src/days/day02.rs
+++ src/days/day02.rs
@@ -1,5 +1,6 @@
 use crate::days::DaySolution;
 use itertools::Itertools;
+use std::cmp::Ordering;
 
 pub struct Day02;
 
@@ -10,21 +11,10 @@ impl DaySolution for Day02 {
     fn solve(&self, input: &str) -> (Option<Self::Output1>, Option<Self::Output2>) {
         let reports = Self::parse(input);
 
-        fn is_safe(levels: impl Iterator<Item = u32>) -> bool {
-            let mut windows = levels.tuple_windows::<(_, _)>().peekable();
-
-            let (first, second) = windows.peek().unwrap();
-            let target_ordering = first.cmp(second);
-
-            windows.all(|(a, b)| {
-                a.cmp(&b) == target_ordering && (1 <= a.abs_diff(b) && a.abs_diff(b) <= 3)
-            })
-        }
-
         // Part 1
         let output1 = reports
             .iter()
-            .filter(|levels| is_safe(levels.iter().copied()))
+            .filter(|levels| Self::is_safe(levels.iter().copied()))
             .count();
 
         // Part 2
@@ -32,7 +22,7 @@ impl DaySolution for Day02 {
             .iter()
             .filter(|levels| {
                 for drop_index in 0..=levels.len() {
-                    if is_safe(
+                    if Self::is_safe(
                         levels
                             .iter()
                             .enumerate()
@@ -67,5 +57,16 @@ impl Day02 {
                     .collect()
             })
             .collect()
+    }
+
+    fn is_safe(levels: impl Iterator<Item = u32> + Clone) -> bool {
+        Self::is_safe_with_ordering(levels.clone(), Ordering::Less)
+            || Self::is_safe_with_ordering(levels.clone(), Ordering::Greater)
+    }
+
+    fn is_safe_with_ordering(levels: impl Iterator<Item = u32>, target_ordering: Ordering) -> bool {
+        levels.tuple_windows().peekable().all(|(a, b)| {
+            a.cmp(&b) == target_ordering && (1 <= a.abs_diff(b) && a.abs_diff(b) <= 3)
+        })
     }
 }
