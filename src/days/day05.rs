086b19c3a7f4debcd95cab446a95f673699aa7b4
Koen Klaren <koen.klaren@gmail.com>
2024-12-05 13:08:36 +0100

    Day 5, Part 2

--- src/days/day05.rs
+++ src/days/day05.rs
@@ -1,12 +1,13 @@
 use crate::days::DaySolution;
 use itertools::Itertools;
+use std::cmp::Ordering;
 use std::collections::HashMap;
 
 pub struct Day05;
 
 impl DaySolution for Day05 {
     type Output1 = u32;
-    type Output2 = ();
+    type Output2 = u32;
 
     fn solve(&self, input: &str) -> (Option<Self::Output1>, Option<Self::Output2>) {
         let mut lines = input.lines();
@@ -26,8 +27,18 @@ impl DaySolution for Day05 {
             rules.entry(page_a).or_default().push(page_b);
         }
 
-        // Part 1
+        let cmp = |a: u32, b: u32| -> Ordering {
+            debug_assert_ne!(a, b);
+            if rules.get(&a).map(|x| x.contains(&b)).unwrap_or(false) {
+                Ordering::Less
+            } else {
+                Ordering::Greater
+            }
+        };
+
         let mut output1 = 0;
+        let mut output2 = 0;
+
         for line in lines {
             let pages = line
                 .split(',')
@@ -37,13 +48,17 @@ impl DaySolution for Day05 {
             if pages
                 .iter()
                 .tuple_combinations()
-                .all(|(a, b)| rules.get(a).map(|x| x.contains(b)).unwrap_or(false))
+                .all(|(a, b)| cmp(*a, *b) == Ordering::Less)
             {
                 output1 += pages[pages.len() / 2];
+            } else {
+                let mut pages = pages;
+                pages.sort_by(|a, b| cmp(*a, *b));
+                output2 += pages[pages.len() / 2];
             }
         }
 
-        (Some(output1), None)
+        (Some(output1), Some(output2))
     }
 }
 
@@ -52,3 +67,5 @@ fn test_day_05() {
     let example = "47|53\n97|13\n97|61\n97|47\n75|29\n61|13\n75|53\n29|13\n97|29\n53|29\n61|53\n97|53\n61|29\n47|13\n75|47\n97|75\n47|61\n75|61\n47|29\n75|13\n53|13\n\n75,47,61,53,29\n97,61,53,29,13\n75,29,13\n75,97,47,61,53\n61,13,29\n97,13,75,29,47";
     assert_eq!(Day05.solve(example), (Some(143), None))
 }
+
+struct PartialOrdSort {}
