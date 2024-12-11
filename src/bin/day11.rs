84e9737f0e8e1e4abb603f55be9c0becca344998
Koen Klaren <koen.klaren@gmail.com>
2024-12-11 20:50:49 +0100

    Day 11, Part 2

--- src/bin/day11.rs
+++ src/bin/day11.rs
@@ -1,24 +1,50 @@
 use advent_of_code_2024::day_main;
 use itertools::Itertools;
+use std::collections::HashMap;
 
 day_main!(11);
 
 fn solve(input: &str) -> (Option<usize>, Option<usize>) {
-    let mut stones = input
+    let stones = input
         .split_whitespace()
         .map(|s| s.parse::<usize>().unwrap())
         .collect_vec();
-    let mut next_stones = vec![];
 
-    for _ in 0..25 {
-        blink(&stones, &mut next_stones);
-        (stones, next_stones) = (next_stones, stones);
-    }
+    let mut cache = HashMap::default();
+    let count1 = stones
+        .iter()
+        .map(|&stone| blink(stone, 25, &mut cache))
+        .sum();
+    let count2 = stones
+        .iter()
+        .map(|&stone| blink(stone, 75, &mut cache))
+        .sum();
 
-    (Some(stones.len()), None)
+    (Some(count1), Some(count2))
 }
 
-fn blink(stones: &Vec<usize>, next_stones: &mut Vec<usize>) {
+fn blink(stone: usize, times: usize, cache: &mut HashMap<(usize, usize), usize>) -> usize {
+    if times == 0 {
+        1
+    } else {
+        if let Some(&result) = cache.get(&(stone, times)) {
+            return result;
+        }
+
+        let result = match stone {
+            0 => blink(1, times - 1, cache),
+            n if (n.ilog10() + 1) % 2 == 0 => {
+                let f = 10usize.pow((n.ilog10() + 1) / 2);
+                blink(n / f, times - 1, cache) + blink(n % f, times - 1, cache)
+            }
+            n => blink(n * 2024, times - 1, cache),
+        };
+        cache.insert((stone, times), result);
+        result
+    }
+}
+
+fn blink_naive(stones: &Vec<usize>, next_stones: &mut Vec<usize>) {
     next_stones.clear();
 
     for &stone in stones {
@@ -36,12 +62,12 @@ fn blink(stones: &Vec<usize>, next_stones: &mut Vec<usize>) {
 
 #[cfg(test)]
 mod tests {
-    use crate::{blink, solve};
+    use crate::{blink_naive, solve};
 
     #[test]
     fn test_solve() {
         let example_input = "125 17";
-        assert_eq!(solve(example_input), (Some(55312), None));
+        assert_eq!(solve(example_input), (Some(55312), Some(65601038650482)));
     }
 
     #[test]
@@ -49,30 +75,30 @@ mod tests {
         let mut stones = vec![125, 17];
         let mut next_stones = vec![];
 
-        blink(&stones, &mut next_stones);
+        blink_naive(&stones, &mut next_stones);
         (next_stones, stones) = (stones, next_stones);
         assert_eq!(stones, vec![253000, 1, 7]);
 
-        blink(&stones, &mut next_stones);
+        blink_naive(&stones, &mut next_stones);
         (next_stones, stones) = (stones, next_stones);
         assert_eq!(stones, vec![253, 0, 2024, 14168]);
 
-        blink(&stones, &mut next_stones);
+        blink_naive(&stones, &mut next_stones);
         (next_stones, stones) = (stones, next_stones);
         assert_eq!(stones, vec![512072, 1, 20, 24, 28676032]);
 
-        blink(&stones, &mut next_stones);
+        blink_naive(&stones, &mut next_stones);
         (next_stones, stones) = (stones, next_stones);
         assert_eq!(stones, vec![512, 72, 2024, 2, 0, 2, 4, 2867, 6032]);
 
-        blink(&stones, &mut next_stones);
+        blink_naive(&stones, &mut next_stones);
         (next_stones, stones) = (stones, next_stones);
         assert_eq!(
             stones,
             vec![1036288, 7, 2, 20, 24, 4048, 1, 4048, 8096, 28, 67, 60, 32]
         );
 
-        blink(&stones, &mut next_stones);
+        blink_naive(&stones, &mut next_stones);
         (_, stones) = (stones, next_stones);
         assert_eq!(
             stones,
