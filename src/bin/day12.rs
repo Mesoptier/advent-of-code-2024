f9bcfe0ad2da40baeecc2e73a368634581f4e6ff
Koen Klaren <koen.klaren@gmail.com>
2024-12-12 17:13:47 +0100

    Day 12, Part 2

--- src/bin/day12.rs
+++ src/bin/day12.rs
@@ -1,5 +1,5 @@
 use advent_of_code_2024::day_main;
-use advent_of_code_2024::util::grid::{Coord, Direction, Grid, StrGrid, VecGrid};
+use advent_of_code_2024::util::grid::{Coord, Grid, StrGrid, VecGrid};
 use std::collections::VecDeque;
 
 day_main!(12);
@@ -9,57 +9,88 @@ fn solve(input: &str) -> (Option<u32>, Option<u32>) {
 
     let mut visited = VecGrid::from_data(grid.width(), vec![false; grid.width() * grid.height()]);
     let mut count1 = 0;
+    let mut count2 = 0;
 
     for (coord, _) in grid.iter() {
         if visited[coord] {
             continue;
         }
 
-        let (area, perimeter) = fill_region(&grid, coord, &mut visited);
+        let (area, perimeter, corners) = fill_region(&grid, coord, &mut visited);
         count1 += area * perimeter;
+        count2 += area * corners;
     }
 
-    (Some(count1), None)
+    (Some(count1), Some(count2))
 }
 
 fn fill_region<'g>(
     grid: &'g impl Grid<'g, Item = &'g u8>,
     coord: Coord,
     visited: &mut VecGrid<bool>,
-) -> (u32, u32) {
+) -> (u32, u32, u32) {
     let mut queue = VecDeque::new();
     queue.push_back(coord);
     visited[coord] = true;
 
     let mut area = 0;
     let mut perimeter = 0;
+    let mut corners = 0;
 
     let target_label = grid.get(coord).unwrap();
+    let is_target_label = |coord: Coord| -> bool { grid.get(coord) == Some(target_label) };
 
     while let Some(coord) = queue.pop_front() {
+        let (x, y) = coord;
+        let min_x = 0;
+        let max_x = grid.width() - 1;
+        let min_y = 0;
+        let max_y = grid.height() - 1;
+
+        // Nearby cells
+        let nc = [
+            min_x < x && min_y < y && is_target_label((x - 1, y - 1)),
+            min_y < y && is_target_label((x, y - 1)),
+            x < max_x && min_y < y && is_target_label((x + 1, y - 1)),
+            min_x < x && is_target_label((x - 1, y)),
+            true,
+            x < max_x && is_target_label((x + 1, y)),
+            min_x < x && y < max_y && is_target_label((x - 1, y + 1)),
+            y < max_y && is_target_label((x, y + 1)),
+            x < max_x && y < max_y && is_target_label((x + 1, y + 1)),
+        ];
+
+        // Count area
         area += 1;
-        perimeter += 4;
 
-        for next_coord in Direction::DIRECTIONS
-            .into_iter()
-            .filter_map(|dir| dir.step(coord, 0, grid.width() - 1, 0, grid.height() - 1))
-        {
-            if grid.get(next_coord).unwrap() != target_label {
-                continue;
+        // Count perimeter + queue next coords
+        for (i, next_coord) in [
+            (1, (x, y.wrapping_sub(1))),
+            (3, (x.wrapping_sub(1), y)),
+            (5, (x + 1, y)),
+            (7, (x, y + 1)),
+        ] {
+            if !nc[i] {
+                perimeter += 1;
+            } else if !visited[next_coord] {
+                visited[next_coord] = true;
+                queue.push_back(next_coord);
             }
+        }
 
-            perimeter -= 1;
-
-            if visited[next_coord] {
-                continue;
+        // Count corners (inner || outer)
+        for [tl, t, l] in [[0, 1, 3], [2, 5, 1], [8, 7, 5], [6, 3, 7]] {
+            // Inner || Outer corner:
+            // .#?      ?.?
+            // ##?      .#?
+            // ???      ???
+            if !nc[tl] && nc[t] && nc[l] || !nc[t] && !nc[l] {
+                corners += 1;
             }
-
-            visited[next_coord] = true;
-            queue.push_back(next_coord);
         }
     }
 
-    (area, perimeter)
+    (area, perimeter, corners)
 }
 
 #[cfg(test)]
@@ -69,6 +100,6 @@ mod tests {
     #[test]
     fn test_solve() {
         let example_input = "RRRRIICCFF\nRRRRIICCCF\nVVRRRCCFFF\nVVRCCCJFFF\nVVVVCJJCFE\nVVIVCCJJEE\nVVIIICJJEE\nMIIIIIJJEE\nMIIISIJEEE\nMMMISSJEEE\n";
-        assert_eq!(solve(example_input), (Some(1930), None));
+        assert_eq!(solve(example_input), (Some(1930), Some(1206)));
     }
 }
