d7336dc5333d3742830f63147d3b5f17a77845a1
Koen Klaren <koen.klaren@gmail.com>
2024-12-13 11:33:49 +0100

    Day 13, Part 2

--- src/bin/day13.rs
+++ src/bin/day13.rs
@@ -13,53 +13,28 @@ fn solve(input: &str) -> (Option<isize>, Option<isize>) {
     let mut input = input;
 
     let mut count1 = 0;
+    let mut count2 = 0;
 
-    while let Ok((next_input, claw_machine)) = parse_claw_machine(input) {
+    while let Ok((next_input, mut claw_machine)) = parse_claw_machine(input) {
         input = next_input;
 
-        // Solve for a, b:
-        // a0 * a + b0 * b = c0
-        // a1 * a + b1 * b = c1
-
-        // Solve for a in terms of b:
-        // a0 * a + b0 * b = c0
-        // a0 * a = c0 - b0 * b
-        // a = (c0 - b0 * b) / a0
-        // a = c0 / a0 - (b0 * b) / a0
-
-        // Substitute for a:
-        // a1 * a + b1 * b = c1
-        // a1 * (c0 / a0 - (b0 * b) / a0) + b1 * b = c1
-        // b = (a1 * c0 - a0 * c1) / (a1 * b0 - a0 * b1)    iff a1 * b0 != a0 * b1 && a0 != 0
-
-        let (a0, a1) = claw_machine.button_a;
-        let (b0, b1) = claw_machine.button_b;
-        let (c0, c1) = claw_machine.prize;
-
-        if a0 == 0 || a1 * b0 == a0 * b1 {
-            // No solution.
-            continue;
+        // Part 1
+        if let Some(cost) = solve_claw_machine(claw_machine) {
+            count1 += cost;
         }
 
-        let (b, b_rem) = (a1 * c0 - a0 * c1).div_rem(a1 * b0 - a0 * b1);
-        if b_rem != 0 {
-            // No integer solution.
-            continue;
+        // Part 2
+        claw_machine.prize.0 += 10000000000000;
+        claw_machine.prize.1 += 10000000000000;
+        if let Some(cost) = solve_claw_machine(claw_machine) {
+            count2 += cost;
         }
-
-        let (a, a_rem) = (c0 - b0 * b).div_rem(a0);
-        if a_rem != 0 {
-            // No integer solution.
-            continue;
-        }
-
-        count1 += a * 3 + b;
     }
 
-    (Some(count1), None)
+    (Some(count1), Some(count2))
 }
 
-#[derive(Debug)]
+#[derive(Debug, Copy, Clone)]
 struct ClawMachine {
     button_a: (isize, isize),
     button_b: (isize, isize),
@@ -95,6 +70,46 @@ fn parse_claw_machine(input: &str) -> IResult<&str, ClawMachine> {
     ))
 }
 
+fn solve_claw_machine(claw_machine: ClawMachine) -> Option<isize> {
+    // Solve for a, b:
+    // a0 * a + b0 * b = c0
+    // a1 * a + b1 * b = c1
+
+    // Solve for a in terms of b:
+    // a0 * a + b0 * b = c0
+    // a0 * a = c0 - b0 * b
+    // a = (c0 - b0 * b) / a0
+    // a = c0 / a0 - (b0 * b) / a0
+
+    // Substitute for a:
+    // a1 * a + b1 * b = c1
+    // a1 * (c0 / a0 - (b0 * b) / a0) + b1 * b = c1
+    // b = (a1 * c0 - a0 * c1) / (a1 * b0 - a0 * b1)    iff a1 * b0 != a0 * b1 && a0 != 0
+
+    let (a0, a1) = claw_machine.button_a;
+    let (b0, b1) = claw_machine.button_b;
+    let (c0, c1) = claw_machine.prize;
+
+    if a0 == 0 || a1 * b0 == a0 * b1 {
+        // No solution.
+        return None;
+    }
+
+    let (b, b_rem) = (a1 * c0 - a0 * c1).div_rem(a1 * b0 - a0 * b1);
+    if b_rem != 0 {
+        // No integer solution.
+        return None;
+    }
+
+    let (a, a_rem) = (c0 - b0 * b).div_rem(a0);
+    if a_rem != 0 {
+        // No integer solution.
+        return None;
+    }
+
+    Some(a * 3 + b)
+}
+
 #[cfg(test)]
 mod tests {
     use crate::solve;
@@ -119,6 +134,6 @@ mod tests {
             Button B: X+27, Y+71
             Prize: X=18641, Y=10279
         "};
-        assert_eq!(solve(example_input), (Some(480), None));
+        assert_eq!(solve(example_input), (Some(480), Some(875318608908)));
     }
 }
